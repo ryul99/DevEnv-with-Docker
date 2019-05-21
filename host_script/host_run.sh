a658f4730fa24d3e7f824022dcbd2955b675fb26
Changmin Choi <cmchoi9901@gmail.com>
2019-05-21 16:16:46 +0900

    make pid1 as init

--- host_script/host_run.sh
+++ host_script/host_run.sh
@@ -17,7 +17,7 @@ printf "image tag : $i1\n"
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
-docker run -it --name "${SUDO_USER}_$i2" \
+docker run -it --init --name "${SUDO_USER}_$i2" \
 --mount type=bind,source="$USER_HOME",target="$USER_HOME" \
 -e LOCAL_USER_ID="$SUDO_UID" -e LOCAL_USER_NAME="$SUDO_USER" \
 -e LOCAL_GROUP_ID="$SUDO_GID" -e LOCAL_USER_HOME="$USER_HOME" \

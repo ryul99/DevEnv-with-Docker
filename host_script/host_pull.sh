75285badd60af062d22dc6fcd39d67f44cd018e1
Changmin Choi <cmchoi9901@gmail.com>
2019-06-24 17:44:43 +0900

    add ipaddress

--- host_script/host_pull.sh
+++ host_script/host_pull.sh
@@ -2,6 +2,7 @@
 
 printf "Enter image tag to pull : "
 read i1
-docker pull "<ipaddress>/${SUDO_USER}:$i1"
-docker image tag "<ipaddress>/${SUDO_USER}:$i1" "${SUDO_USER}:$i1"
-docker rmi "<ipaddress>/${SUDO_USER}:$i1"
+ipaddress='192.168.128.210:5000'
+docker pull "$ipaddress/${SUDO_USER}:$i1"
+docker image tag "$ipaddress/${SUDO_USER}:$i1" "${SUDO_USER}:$i1"
+docker rmi "$ipaddress/${SUDO_USER}:$i1"
--- host_script/host_push.sh
+++ host_script/host_push.sh
@@ -2,6 +2,7 @@
 
 printf "Enter image tag to push : "
 read i1
-docker image tag "${SUDO_USER}:$i1" "<ipaddress>/${SUDO_USER}:$i1"
-docker push "<ipaddress>/${SUDO_USER}:$i1"
-docker rmi "<ipaddress>/${SUDO_USER}:$i1"
+ipaddress='192.168.128.210:5000'
+docker image tag "${SUDO_USER}:$i1" "$ipaddress/${SUDO_USER}:$i1"
+docker push "$ipaddress/${SUDO_USER}:$i1"
+docker rmi "$ipaddress/${SUDO_USER}:$i1"

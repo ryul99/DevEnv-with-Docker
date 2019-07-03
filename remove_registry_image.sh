14ee9f693503de745216994cf2974da80985dfbf
Changmin Choi <cmchoi9901@gmail.com>
2019-07-03 10:35:33 +0900

    remove_registry_image.sh gets image tag

--- remove_registry_image.sh
+++ remove_registry_image.sh
@@ -1,12 +1,12 @@
 printf "Enter image name to remove : "
 read name
+printf "Enter image tag to remove : "
+read tag
 registry='192.168.128.210:5000'
 curl -v -sSL -X DELETE "http://${registry}/v2/${name}/manifests/$(
     curl -sSL -I \
         -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-        "http://${registry}/v2/${name}/manifests/$(
-            curl -sSL "http://${registry}/v2/${name}/tags/list" | jq -r '.tags[0]'
-        )" \
+        "http://${registry}/v2/${name}/manifests/${tag}" \
     | awk '$1 == "Docker-Content-Digest:" { print $2 }' \
     | tr -d $'\r' \
 )"

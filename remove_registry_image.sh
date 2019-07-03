7b3a2dd7ef0ce6af5e8d7b056bd4f33de162e78f
Changmin Choi <cmchoi9901@gmail.com>
2019-07-03 10:18:03 +0900

    add remove_registry_image.sh

---
+++ remove_registry_image.sh
@@ -0,0 +1,13 @@
+printf "Enter image name to remove : "
+read name
+registry='192.168.128.210:5000'
+curl -v -sSL -X DELETE "http://${registry}/v2/${name}/manifests/$(
+    curl -sSL -I \
+        -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+        "http://${registry}/v2/${name}/manifests/$(
+            curl -sSL "http://${registry}/v2/${name}/tags/list" | jq -r '.tags[0]'
+        )" \
+    | awk '$1 == "Docker-Content-Digest:" { print $2 }' \
+    | tr -d $'\r' \
+)"
+printf "Please run 'docker exec -it docker-registry bin/registry garbage-collect /etc/docker/registry/config.yml'"

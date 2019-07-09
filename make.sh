aec59a57dac2280758c926ff9b0d68f7fd1381df
Changmin Choi <cmchoi9901@gmail.com>
2019-07-09 13:32:34 +0900

    make base image contains registry server

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ipaddress='192.168.128.210:5000'
 
 docker build -t centos-essential ./base
-docker build -t base ./fully
+docker build -t "${ipaddress}/base" ./fully
+docker rmi centos-essential

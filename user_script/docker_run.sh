eb95bcb54c5ebe398efb64454ed031402285b135
Changmin Choi <cmchoi9901@gmail.com>
2020-06-01 13:18:48 +0900

    add LANG UTF-8 and set same network as host

--- user_script/docker_run.sh
+++ user_script/docker_run.sh
@@ -6,4 +6,5 @@ docker run -it --init --name "$2" \
     --mount type=bind,source="$USER_HOME",target="$USER_HOME" \
     -e LOCAL_USER_ID="$SUDO_UID" -e LOCAL_USER_NAME="$SUDO_USER" \
     -e LOCAL_GROUP_ID="$SUDO_GID" -e LOCAL_USER_HOME="$USER_HOME" \
+    -e LC_ALL=C.UTF-8 -e LANG=C.UTF-8 -it --network host \
     "$1"

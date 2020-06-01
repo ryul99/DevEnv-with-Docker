ab79c255710012eb2b0d22c7d75b572355ec6ae5
Changmin Choi <cmchoi9901@gmail.com>
2020-06-01 11:32:52 +0900

    remove legacy of SNUH

--- make.sh
+++ make.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-ipaddress='192.168.128.210:5000'
-
-docker build -t centos-essential ./base
-docker build -t "${ipaddress}/base" ./fully
-docker rmi centos-essential
+docker build -t essential ./base
+docker build -t fully ./fully

8278cf3194b1bab7c636ba8ca0e4821452316a49
Changmin Choi <cmchoi9901@gmail.com>
2020-06-05 17:35:43 +0900

    able to run user_script without sudo

--- user_script/docker_exec.sh
+++ user_script/docker_exec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "exec '$2' in '$1' container"
-docker exec -it -u $SUDO_USER "$1" "$2"
+R_USER="${SUDO_USER:-${USER}}"
+docker exec -it -u $R_USER "$1" "$2"
--- user_script/docker_run.sh
+++ user_script/docker_run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Run '$1' image as '$2' container"
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+R_USER="${SUDO_USER:-${USER}}"
+R_UID="${SUDO_UID:-${UID}}"
+R_GID="${SUDO_GID:-$(id -g "$R_USER")}"
+USER_HOME=$(getent passwd $R_USER | cut -d: -f6)
 docker run -it --init --name "$2" \
     --mount type=bind,source="$USER_HOME",target="$USER_HOME" \
-    -e LOCAL_USER_ID="$SUDO_UID" -e LOCAL_USER_NAME="$SUDO_USER" \
-    -e LOCAL_GROUP_ID="$SUDO_GID" -e LOCAL_USER_HOME="$USER_HOME" \
-    -e LC_ALL=C.UTF-8 -e LANG=C.UTF-8 -it --network host \
+    -e LOCAL_USER_ID="$R_UID" -e LOCAL_USER_NAME="$R_USER" \
+    -e LOCAL_GROUP_ID="$R_GID" -e LOCAL_USER_HOME="$USER_HOME" \
+    -e LC_ALL="C.UTF-8" -e LANG="C.UTF-8" -it --network host \
     "$1"
